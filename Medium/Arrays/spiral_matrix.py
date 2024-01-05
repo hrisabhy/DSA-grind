3d1b6d5b9233f53f0bde4e1c2d023d4a46d51d5b
hrisabhy <hrisabhyadav31@gmail.com>
2024-01-05 11:02:49 +0530

    Modified spiral matrix

--- Medium/Arrays/spiral_matrix.py
+++ Medium/Arrays/spiral_matrix.py
@@ -1,4 +1,8 @@
 def printSpiral(matrix):
+    """
+    Time Complexity: O(m x n) { Since all the elements are being traversed once and there are total n x m elements ( m elements in each row and total n rows) so the time complexity will be O(n x m)}.
+    Space Complexity: O(n) { Extra Space used for storing traversal in the ans array }.
+    """
     top = 0
     bottom = len(matrix) - 1
     right = len(matrix[0]) - 1
